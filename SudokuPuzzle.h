ee5c645a2a4a56859ce168c2c15d770bd3f3e85a
TheNoriBBy <151388537+TheNoriBBy@users.noreply.github.com>
2024-11-22 23:56:22 +0100

    Update SudokuPuzzle.h

--- SudokuPuzzle.h
+++ SudokuPuzzle.h
@@ -1,1 +1,22 @@
+#ifndef _SUDOKU_PUZZLE_H
+#define _SUDOKU_PUZZLE_H
 
+class SudokuPuzzle {
+private:
+    short int board[9][9];
+    bool debug;
+
+    void printTracerTryingValue(int x_cord, int y_cord);
+    bool solve(int x_cord, int y_cord);
+    bool proceedToNext(int x_cord, int y_cord);
+    bool verifyValue(int x_cord, int y_cord);
+
+public:
+    SudokuPuzzle();
+    void print() const;
+    void setBoardValue(int x_cord, int y_cord, int value);
+    int getBoardValue(int x_cord, int y_cord) const;
+    bool solve();
+};
+
+#endif
