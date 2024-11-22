e520a16d4e8fac74172e11a154d80a05eb508dae
TheNoriBBy <151388537+TheNoriBBy@users.noreply.github.com>
2024-11-23 00:01:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "SudokuPuzzle.h"
 
+int main(int argc, char* const argv[]) {
+    SudokuPuzzle puzzle;
+
+    puzzle.generate(); 
+    puzzle.print(); 
+
+    std::cout << std::endl;
+
+    if (puzzle.solve()) {
+        std::cout << "Here is the solution:" << std::endl;
+        puzzle.print();
+    }
+    else {
+        std::cout << "Puzzle is not solvable.";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
